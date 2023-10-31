aef7fa61d8bbc677874206cb97623221740d3753
menglong <my15754531361@163.com>
2023-10-31 14:06:52 +0800

    1~5
    1.配置
    ①启动项目自动打开网页
    ②关闭eslint
    2.实现非路由组件
    ①实现TheHeader组件
    ②实现TheFooter组件
    ③引入reset.css清空默认样式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <the-header></the-header>
+    <the-footer></the-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader'
+import TheFooter from './components/TheFooter'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader,
+    TheFooter,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
