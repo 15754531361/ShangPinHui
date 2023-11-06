4865f62b5763a1ffe594050ba281d9e77534ff3c
menglong <my15754531361@163.com>
2023-11-06 13:46:55 +0800

    9~12_style_01
    1.格式化/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -8,20 +8,30 @@ import AppLogin from '@/pages/AppLogin'
 import AppRegister from '@/pages/AppRegister'
 import AppSearch from '@/pages/AppSearch'
 
-let originPush=VueRouter.prototype.push
-let originReplace=VueRouter.prototype.replace
-VueRouter.prototype.push=function(location,resolve,reject){
-  if(resolve && reject){
-    originPush.call(this,location,resolve,reject)
-  }else{
-    originPush.call(this,location,()=>{},()=>{})
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+VueRouter.prototype.push = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originPush.call(this, location, resolve, reject)
+  } else {
+    originPush.call(
+      this,
+      location,
+      () => {},
+      () => {}
+    )
   }
 }
-VueRouter.prototype.replace=function(location,resolve,reject){
-  if(resolve && reject){
-    originReplace.call(this,location,resolve,reject)
-  }else{
-    originReplace.call(this,location,()=>{},()=>{})
+VueRouter.prototype.replace = function (location, resolve, reject) {
+  if (resolve && reject) {
+    originReplace.call(this, location, resolve, reject)
+  } else {
+    originReplace.call(
+      this,
+      location,
+      () => {},
+      () => {}
+    )
   }
 }
 
